7f40fec83835028079189114cb8a4af5b4480a4a
Binh Vu <binh@toan2.com>
2018-01-31 02:08:08 -0700

    update setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,14 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-from distutils.core import setup
+from distutils.core import setup, Extension
 from setuptools import find_packages
+from Cython.Build import cythonize
+
+ext = Extension(name="sparsehash",
+         sources=["sparsehash/dense_hash_map.pyx", "sparsehash/dense_hash_set.pyx"],
+         language='c++',
+         extra_compile_args=['-std=c++14', '-stdlib=libc++'])
 
 setup(
     name='sparsehash',
@@ -11,5 +17,6 @@ setup(
     author='Binh Vu',
     author_email='binhlvu@gmail.com',
     url='https://github.com/binh-vu/pyutils',
+    ext_modules=cythonize(ext),
     packages=find_packages(exclude=['tests.*', 'tests'])
 )
